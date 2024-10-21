2020e0e14efce492fe8d67dab77bb0fe9411d06d
Krushna Kharat <krushnakharat.official@gmail.com>
2024-10-21 22:21:24 +0530

    hehe

--- recidencyServices/src/app/profiles/user/userlogin/userlogin.component.ts
+++ recidencyServices/src/app/profiles/user/userlogin/userlogin.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { UserregisterComponent } from '../userregister/userregister.component';
 
 @Component({
   selector: 'app-userlogin',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, UserregisterComponent , UserloginComponent , RouterOutlet],
   templateUrl: './userlogin.component.html',
   styleUrl: './userlogin.component.css'
 })
